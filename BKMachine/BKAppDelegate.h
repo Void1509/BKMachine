33fd421037ff714e33b1feafffcde51b70521827
Valeriy <valeriy@svaMacBook.local>
2012-11-15 12:17:30 +0200

    Initial Commit

---
+++ BKMachine/BKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BKAppDelegate.h
+//  BKMachine
+//
+//  Created by Valeriy on 15.11.12.
+//  Copyright (c) 2012 Valeriy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BKAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ BKMachine/BKAppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  BKAppDelegate.m
+//  BKMachine
+//
+//  Created by Valeriy on 15.11.12.
+//  Copyright (c) 2012 Valeriy. All rights reserved.
+//
+
+#import "BKAppDelegate.h"
+
+@implementation BKAppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
